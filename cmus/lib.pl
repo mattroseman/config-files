a3b0d0cb1f59f5ddd438a6b1fa04014fa2a9aeec
Matthew Roseman <mroseman95@gmail.com>
2017-03-31 17:53:37 -0400

    initial commit
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

---
+++ cmus/lib.pl
@@ -0,0 +1,16 @@
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/03_Boards of Canada - Into The Rainbow Vein.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/09_Boards of Canada - Chromakey Dreamcoat.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/17_Boards of Canada - Satellite Anthem Icarus.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/21_Boards of Canada - Peacock Tail.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/25_Boards of Canada - Dayvan Cowboy.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/30_Boards of Canada - A Moment Of Clarity.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/35_Boards of Canada - '84 Pontiac Dream.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/39_Boards of Canada - Sherbet Head.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/42_Boards of Canada - Oscar See Through Red Eye.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/43_Boards of Canada - Ataronchronon.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/45_Boards of Canada - Hey Saturday Sun.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/48_Boards of Canada - Constants Are Changing.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/50_Boards of Canada - Slow This Bird Down.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/52_Boards of Canada - Tears From The Compounded Eye.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/53_Boards of Canada - Farewell Fire.mp3
+/home/matthew/Music/BoardsOfCanada/Boards of Canada - 2005 - The Campfire Headphase/55_Boards Of Canada - Macquarie Ridge.mp3
